4c269ffca26102512f4641e2891b65d4ea848960
Mehdi <rame3116@gmail.com>
2019-11-05 08:27:41 -0500

    Switches, doors

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour
+{
+    public int LevelToLoad;
+    public bool doorOpen = true;
+    private void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+        if (sceneName == "SoundManagerSample") // Si le niveau contient des interrupteurs, on le met ici
+        {
+            doorOpen = false;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (doorOpen)
+        {
+
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+                {
+                    SceneManager.LoadScene(LevelToLoad);
+
+                }
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (doorOpen)
+        {
+
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+                {
+                    //SceneManager.LoadScene(LevelToLoad);
+                    SceneManager.LoadScene("SoundManagerSample");
+
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/switches.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switches : MonoBehaviour
+{
+    public door DoorToOpen;
+
+    void Start()
+    {
+        GameObject g = GameObject.FindGameObjectWithTag("Door");
+        //assigns the script component "IGotBools" to the public variable of type "IGotBools" names boolBoy.
+        DoorToOpen = g.GetComponent<door>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DoorToOpen.doorOpen = true;
+        }
+        
+    }
+}
+    
