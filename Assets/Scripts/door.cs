6fb85f0200f91155fbf2a0952535513cec1034a1
Mehdi <rame3116@gmail.com>
2019-11-05 08:38:23 -0500

    Useless thing removed

--- Assets/Scripts/door.cs
+++ Assets/Scripts/door.cs
@@ -7,17 +7,7 @@ public class door : MonoBehaviour
 {
     public int LevelToLoad;
     public bool doorOpen = true;
-    private void Start()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
 
-        string sceneName = currentScene.name;
-        if (sceneName == "SoundManagerSample") // Si le niveau contient des interrupteurs, on le met ici
-        {
-            doorOpen = false;
-
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
