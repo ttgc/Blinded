96af21fc9f75de8e5320a23aba1d5f989ac26cc9
ttgc <ttgc@users.noreply.github.com>
2019-12-01 18:44:42 -0500

    Fixed sound in ending

--- Assets/Scripts/endScript.cs
+++ Assets/Scripts/endScript.cs
@@ -12,8 +12,7 @@ public class endScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        scriptSource = SoundManager.instance.efxSource;
-        SoundManager.instance.PlayClip(finalScript, new Vector3(0, 0));
+        scriptSource = SoundManager.instance.PlayClip(finalScript, new Vector3(0, 0));
     }
 
     // Update is called once per frame
