6adadf65052577006f44570a27172488d0aa9783
ttgc <ttgc@users.noreply.github.com>
2019-12-01 17:32:33 -0500

    Added ending

---
+++ Assets/Scripts/endScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endScript : MonoBehaviour
+{
+    private AudioSource scriptSource;
+    private bool finished = false;
+
+    public AudioClip finalScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scriptSource = SoundManager.instance.efxSource;
+        SoundManager.instance.PlayClip(finalScript, new Vector3(0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!finished && !scriptSource.isPlaying)
+        {
+            finished = true;
+            // THE END
+            Debug.Break();
+        }
+    }
+}
