557c6e0e3c0de86e1ec8c78ef77b70948d814f66
ttgc <ttgc@users.noreply.github.com>
2019-12-02 14:51:27 -0500

    Added box sound

--- Assets/Scripts/boxpull.cs
+++ Assets/Scripts/boxpull.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class boxpull : MonoBehaviour
 {
     public bool beingPushed;
+    public AudioClip collisionSound;
     float xPos;
      
     void Start()
@@ -24,5 +25,13 @@ public class boxpull : MonoBehaviour
             xPos = transform.position.x;
         }
                        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SoundManager.instance.PlayClip(collisionSound, GetComponent<Transform>().position);
+        }
     }
 }
